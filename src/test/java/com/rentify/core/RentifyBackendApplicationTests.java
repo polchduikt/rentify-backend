bfd0bb27184bed2d568fdf7666e01fb9355104b5
polchduikt <polchilliakney@gmail.com>
2026-03-02 16:28:56 +0200

    Spring Boot project scaffolding

---
+++ src/test/java/com/rentify/core/RentifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentify.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentifyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
